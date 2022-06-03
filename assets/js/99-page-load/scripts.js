ea0de9a3aeeb066b61f410db2828992048c729ab
apbarry30 <apbarry30@miami.edu>
2022-06-03 12:46:28 -0400

    fix

--- assets/js/99-page-load/scripts.js
+++ assets/js/99-page-load/scripts.js
@@ -4,24 +4,12 @@
 
 jQuery(document).ready(function($) {
 
-    // OWL CAROUSEL
-
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 2000,
-        pagination: false,
-        nav: false,
-        rewindSpeed: 500
-    });
-
-
     // Mobile Header
     $(".hamburger").on("click", function() {
         $(this).toggleClass("open");
         $(".site-content").toggleClass("open");
         $(".nav-menu").toggleClass("open");
+        $('body').toggleClass('fixed');
     });
 
 
