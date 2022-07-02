1625fd751544bd481150ed74a9b89f4286c9638b
amandaogk <42657166+apbarry30@users.noreply.github.com>
2022-07-02 15:06:05 -0400

    adding about me page

--- assets/js/99-page-load/scripts.js
+++ assets/js/99-page-load/scripts.js
@@ -11,9 +11,41 @@ jQuery(document).ready(function($) {
         $(".nav-menu").toggleClass("open");
         $('body').toggleClass('fixed');
     });
+    // IN VIEW ANIMATIONS
+    let fadeInSpeed = 700;
+
+    $('.inview-fadeUp').on('inview', function (event, isInView) {
+        if (isInView) {
+            // element is now visible in the viewport
+            $(this).addClass("fadeUp");
+        } else {
+            // element has gone out of viewport
+        }
+    });
 
 
+    $('.inview-fadeIn').on('inview', function (event, isInView) {
+        if (isInView) {
+            // element is now visible in the viewport
+            $(this).addClass("fadeIn");
+        } else {
+            // element has gone out of viewport
+        }
+    });
+
+    $('.portfolio-item').on('click', function(){
+       target =  $(this).data('name');
+       openPop = "." + target;
+       memberPopup = $('.port-popup');
+       $(openPop).addClass('open');
+       $('body').addClass("fixed");
+    })
+    $('body.fixed').on('click', function(){
+        $('body').removeClass('fixed');
+        $('.port-popup').removeClass('open');
+    })
 
 
 });
+
 
