07f4da95c692101dfbb47ad2a9002e7b0cdb8703
amandaogk <42657166+apbarry30@users.noreply.github.com>
2023-02-13 13:52:23 -0500

    updates

--- assets/js/99-page-load/scripts.js
+++ assets/js/99-page-load/scripts.js
@@ -3,63 +3,7 @@
  */
 
 jQuery(document).ready(function($) {
-        //slider
-    $('.slider').slick({
-            dots:false,
-            slidesToScroll:1,
-            slidesToShow: 1,
-            prevArrow: $('.prev'),
-            nextArrow: $('.next'),
-            centerPadding:50,
-            centerMode: true,
-    });
 
-    $('.button--bubble').each(function() {
-        var $circlesTopLeft = $(this).parent().find('.circle.top-left');
-        var $circlesBottomRight = $(this).parent().find('.circle.bottom-right');
-
-        var tl = new TimelineLite();
-        var tl2 = new TimelineLite();
-
-        var btTl = new TimelineLite({ paused: true });
-
-        tl.to($circlesTopLeft, 1.2, { x: -25, y: -25, scaleY: 2, ease: SlowMo.ease.config(0.1, 0.7, false) });
-        tl.to($circlesTopLeft.eq(0), 0.1, { scale: 0.2, x: '+=6', y: '-=2' });
-        tl.to($circlesTopLeft.eq(1), 0.1, { scaleX: 1, scaleY: 0.8, x: '-=10', y: '-=7' }, '-=0.1');
-        tl.to($circlesTopLeft.eq(2), 0.1, { scale: 0.2, x: '-=15', y: '+=6' }, '-=0.1');
-        tl.to($circlesTopLeft.eq(0), 1, { scale: 0, x: '-=5', y: '-=15', opacity: 0 });
-        tl.to($circlesTopLeft.eq(1), 1, { scaleX: 0.4, scaleY: 0.4, x: '-=10', y: '-=10', opacity: 0 }, '-=1');
-        tl.to($circlesTopLeft.eq(2), 1, { scale: 0, x: '-=15', y: '+=5', opacity: 0 }, '-=1');
-
-        var tlBt1 = new TimelineLite();
-        var tlBt2 = new TimelineLite();
-
-        tlBt1.set($circlesTopLeft, { x: 0, y: 0, rotation: -45 });
-        tlBt1.add(tl);
-
-        tl2.set($circlesBottomRight, { x: 0, y: 0 });
-        tl2.to($circlesBottomRight, 1.1, { x: 30, y: 30, ease: SlowMo.ease.config(0.1, 0.7, false) });
-        tl2.to($circlesBottomRight.eq(0), 0.1, { scale: 0.2, x: '-=6', y: '+=3' });
-        tl2.to($circlesBottomRight.eq(1), 0.1, { scale: 0.8, x: '+=7', y: '+=3' }, '-=0.1');
-        tl2.to($circlesBottomRight.eq(2), 0.1, { scale: 0.2, x: '+=15', y: '-=6' }, '-=0.2');
-        tl2.to($circlesBottomRight.eq(0), 1, { scale: 0, x: '+=5', y: '+=15', opacity: 0 });
-        tl2.to($circlesBottomRight.eq(1), 1, { scale: 0.4, x: '+=7', y: '+=7', opacity: 0 }, '-=1');
-        tl2.to($circlesBottomRight.eq(2), 1, { scale: 0, x: '+=15', y: '-=5', opacity: 0 }, '-=1');
-
-        tlBt2.set($circlesBottomRight, { x: 0, y: 0, rotation: 45 });
-        tlBt2.add(tl2);
-
-        btTl.add(tlBt1);
-        btTl.to($(this).parent().find('.button.effect-button'), 0.8, { scaleY: 1.1 }, 0.1);
-        btTl.add(tlBt2, 0.2);
-        btTl.to($(this).parent().find('.button.effect-button'), 1.8, { scale: 1, ease: Elastic.easeOut.config(1.2, 0.4) }, 1.2);
-
-        btTl.timeScale(2.6);
-
-        $(this).on('mouseover', function() {
-            btTl.restart();
-        });
-    });
 
     // Mobile Header
     $(".hamburger").on("click", function() {
@@ -68,8 +12,7 @@ jQuery(document).ready(function($) {
         $(".nav-menu").toggleClass("open");
         $('body').toggleClass('fixed');
     });
-    // IN VIEW ANIMATIONS
-    let fadeInSpeed = 700;
+    fadeInSpeed = 700;
 
     $('.inview-fadeUp').on('inview', function (event, isInView) {
         if (isInView) {
@@ -80,19 +23,11 @@ jQuery(document).ready(function($) {
         }
     });
 
-    // $('.projects-text').on('inview', function(event, IsInView){
-    //     if (isInView) {
-    //         // element is now visible in the viewport
-    //         $(this).addClass('fixed');
-    //     } else {
-    //         // element has gone out of viewport
-    //     }
-    // });
 
     $('.portfolio-item').on('click', function(){
-       target =  $(this).data('name');
-       openPop = "." + target;
-       memberPopup = $('.port-popup');
+       var target =  $(this).data('name');
+       var openPop = "." + target;
+       var memberPopup = $('.port-popup');
        $(openPop).addClass('open');
        $('body').addClass("fixed");
     });
@@ -107,8 +42,8 @@ jQuery(document).ready(function($) {
 
     $('.nav-name').on('mouseover', function(){
         $('.animate').removeClass('active');
-        target =  $(this).data('name');
-        navItem = "." + target;
+        var target =  $(this).data('name');
+        var navItem = "." + target;
         $(navItem).addClass('active');
     });
     $('.nav-name').on('mouseleave', function(){
@@ -126,10 +61,37 @@ jQuery(document).ready(function($) {
 
 
 
+});
 
-});
+var scrollObject = {
+    y: window.pageYOffset
+}
+
+var getScrollPosition = (e) => {
+
+    var prevY = scrollObject.y
+
+    scrollObject = {
+        y: window.pageYOffset
+    }
+
+    if(scrollObject.y > 5) {
+        document.body.classList.add('scrolled')
+        if(prevY >= scrollObject.y) {
+            document.body.classList.add('scrolled-up')
+        }
+        else {
+            document.body.classList.remove('scrolled-up')
+        }
+    } else {
+        document.body.classList.remove('scrolled','scrolled-up')
+    }
+}
+
+window.onscroll = getScrollPosition
+
 //default JS Setting
-var rellax = new Rellax('.rellax', {
-    breakpoints:[576, 768, 1201]
-});
+// var rellax = new Rellax('.rellax', {
+//     breakpoints:[576, 768, 1201]
+// });
 
